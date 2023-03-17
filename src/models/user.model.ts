520ef7bd7a978605caae82249916531c80c5ca40
lucascbb <lucasbaroquello@hotmail.com>
2023-03-16 21:41:57 -0300

    change name by id

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,21 +1,20 @@
 import { ResultSetHeader } from 'mysql2';
-import { IUser, IToken } from '../interfaces';
+import { IUser, IToken, IUserId } from '../interfaces';
 import connection from './connection';
 import { newToken } from '../utils/token.validate';
 
 export const createUser = async (user: IUser): Promise<IToken> => {
   const { username, vocation, level, password } = user;
 
-  // const sql2 = 'SELECT id FROM Trybesmith.users order by id desc limit 1';
-  // const [rows] = await connection.execute(sql2);
+  const sql2 = 'SELECT id FROM Trybesmith.users order by id desc limit 1';
   
+  const [rows] = await connection.execute(sql2);
+  const [id] = rows as IUserId[];
+
   const sql = 'INSERT INTO Trybesmith.users (username, vocation, level, password) VALUES (?,?,?,?)';
   await connection.execute<ResultSetHeader>(sql, [username, vocation, level, password]);
 
-  const token = newToken(username);
-  // console.log(a);
-  
-  // const token = 'fdfsfsdfsd.fsfsdfds.sdfsd45';
+  const token = newToken(id);
 
   const newUser: IToken = { token };
   return newUser;
--- src/utils/token.validate.ts
+++ src/utils/token.validate.ts
@@ -4,7 +4,7 @@ const secret = process.env.JWT_SECRET || 'secret';
 
 const jwtConfig:object = { expiresIn: '7d', algorithm: 'HS256' };
 
-export const newToken = (username:string) => jwt.sign({ 
+export const newToken = (username:object) => jwt.sign({ 
   data: { userName: username } }, secret, jwtConfig);
 
 export const validateToken = (token:string) => jwt.verify(token, secret);
