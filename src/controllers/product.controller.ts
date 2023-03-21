af7b6effaea551aac981a9b225805c11708e60f5
lucascbb <lucasbaroquello@hotmail.com>
2023-03-21 12:31:15 -0300

    req 6

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -11,6 +11,10 @@ export const createProduct = async (req:Request, res:Response) => {
 
   if (!name) return res.status(400).json({ message: '"name" is required' });
   if (typeof name !== 'string') return res.status(422).json({ message: '"name" must be a string' });
+  if (name.length > 2) {
+    return res.status(422).json({ 
+      message: '"name" length must be at least 3 characters long' });
+  }
   
   const productCreated = await productService.createProduct(name, amount);
   return res.status(201).json(productCreated);
