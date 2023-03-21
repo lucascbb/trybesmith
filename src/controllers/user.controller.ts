76fd10126f2808413dd1746092a114cc29ef43e9
lucascbb <lucasbaroquello@hotmail.com>
2023-03-21 14:32:25 -0300

    req 7

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import userService from '../services/user.service';
 import { validateUsername } from '../utils/validate.username';
+import { validateVocation } from '../utils/validate.vocation';
+import { validateLevel } from '../utils/validate.level';
 
 const createUser = async (req:Request, res:Response) => {
   const user = req.body;
@@ -8,6 +10,12 @@ const createUser = async (req:Request, res:Response) => {
 
   const valiUser = validateUsername(username);
   if (valiUser) return res.status(valiUser.status).json({ message: valiUser.message });
+  
+  const valiVocation = validateVocation(vocation);
+  if (valiVocation) return res.status(valiVocation.status).json({ message: valiVocation.message });
+
+  const valiLevel = validateLevel(level);
+  if (valiLevel) return res.status(valiLevel.status).json({ message: valiLevel.message });
 
   const token = await userService.createUser(user);
   return res.status(201).json(token);
