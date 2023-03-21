b8f6dbff6a71d57e3b44525b9078777015390402
lucascbb <lucasbaroquello@hotmail.com>
2023-03-21 14:26:31 -0300

    req 7

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express';
-
 import userService from '../services/user.service';
+import { validateUsername } from '../utils/validate.username';
 
 const createUser = async (req:Request, res:Response) => {
   const user = req.body;
+  const { username, vocation, level, password } = req.body;
+
+  const valiUser = validateUsername(username);
+  if (valiUser) return res.status(valiUser.status).json({ message: valiUser.message });
 
   const token = await userService.createUser(user);
   return res.status(201).json(token);
--- src/utils/validate.username.ts
+++ src/utils/validate.username.ts
@@ -1,10 +1,10 @@
-export const validateUsername = (amount:string) => {
-  if (!amount) return { status: 400, message: '"amount" is required' };
-  if (typeof amount !== 'string') return { status: 422, message: '"amount" must be a string' };
-  if (amount.length < 3) { 
+export const validateUsername = (username:string) => {
+  if (!username) return { status: 400, message: '"username" is required' };
+  if (typeof username !== 'string') return { status: 422, message: '"username" must be a string' };
+  if (username.length < 3) { 
     return { 
       status: 422, 
-      message: '"amount" length must be at least 3 characters long' }; 
+      message: '"username" length must be at least 3 characters long' }; 
   }
 };
 
